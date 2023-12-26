cc3543e32fa7e2c74947af34ba4adfdb593b62d9
THotarek <tomas.hotarek@iotor.io>
2023-12-26 23:05:53 +0100

    m

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -46,7 +46,8 @@ axiosInstance.interceptors.response.use(
 );
 
 // const axi_url = "http://localhost:3001/";
-const axi_url = "https://37.46.208.164:3001/";
+// const axi_url = "https://37.46.208.164:3001/";
+const axi_url = "https://wellick.cz/";
 
 axiosInstance.getSets = function () {
   return this.get(axi_url + "api/sets"); // replace with your API endpoint
